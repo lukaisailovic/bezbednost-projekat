7b71a259e7a1c1aa8626cfeeb561fce5bcd25134
Luka Isailovic <lukaisailovic@gmail.com>
2021-05-16 20:37:07 +0200

    Change client and server not to be utility classes

--- src/app/client/Client.java
+++ src/app/client/Client.java
@@ -3,23 +3,20 @@ package app.client;
 import app.shared.request.Request;
 import app.shared.request.RequestType;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
 public class Client {
     private static final int SERVER_PORT = 2021;
     private static final String SERVER_HOST = "localhost";
-
+    private final Socket socket;
 
     public Client() throws Exception {
-        Socket socket = new Socket(SERVER_HOST, SERVER_PORT);
+        this.socket = new Socket(SERVER_HOST, SERVER_PORT);
+    }
 
-
+    public void start() throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        BufferedReader tin = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
 
         System.out.println("Connected to server on port "+SERVER_PORT);
@@ -44,12 +41,12 @@ public class Client {
 
         socket.close();
         System.out.println("Disconnecting");
-
     }
 
     public static void main(String[] args) {
         try {
-            new Client();
+            Client client = new Client();
+            client.start();
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/app/server/Server.java
+++ src/app/server/Server.java
@@ -1,30 +1,31 @@
 package app.server;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
 
     private static final int PORT = 2021;
+    private final ServerSocket serverSocket;
+
 
     public Server() throws Exception {
-        ServerSocket serverSocket = new ServerSocket(PORT);
-
+        this.serverSocket = new ServerSocket(PORT);
+    }
+    public void start() throws IOException {
+        System.out.println("Server waiting for connections on port "+PORT);
         while (true) {
-
             Socket socket = serverSocket.accept();
-
             ServerThread serverThread = new ServerThread(socket, this);
-
             Thread thread = new Thread(serverThread);
-
             thread.start();
         }
-
     }
     public static void main(String[] args) {
         try {
-            new Server();
+            Server server = new Server();
+            server.start();
         } catch (Exception e) {
             e.printStackTrace();
         }
